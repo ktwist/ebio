845e337fe353e694a96ee0ada2c738a54c2d05c7
Edin Raimovic <edimust@gmail.com>
2019-10-20 16:42:58 +0200

    first commitinitial commit - react app setup, tailwind css added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App" >
+    <div className="w-full max-w-md bg-gray-800" >
+      <div action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
+        <div className="px-4 pb-4">
+          <h2 className="text-sm block font-bold pb-2">Hello!</h2>
+          <p>It Works</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
